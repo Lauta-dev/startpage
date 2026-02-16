69e41fc4929697dfa08367a225f58d5a8dd977df
lauta-dev <altairdiaz245@gmail.com>
2026-02-16 17:10:00 -0300

    edito la query sql para obtener el id del bookmark

--- actions/getBookmark.ts
+++ actions/getBookmark.ts
@@ -1,9 +1,4 @@
-import { createClient } from '@libsql/client';
-
-const client = createClient({
-  url: process.env.TURSO_DATABASE_URL || "",
-  authToken: process.env.TURSO_AUTH_TOKEN || ""
-});
+import client from "@/lib/db";
 
 export default async function Aaa() {
       const query = `
@@ -13,12 +8,12 @@ export default async function Aaa() {
       b.id as bookmark_id,
       b.name as bookmark_name,
       b.icon as bookmark_icon,
-      b.site as bookmark_site
+      b.site as bookmark_site,
+      b.id   as bookmark_id
     FROM categories c
     LEFT JOIN bookmarks b ON c.id = b.category_id
     ORDER BY c.display_order, b.display_order
   `;
-
 
   const result = await client.execute(query);
   
@@ -37,10 +32,16 @@ export default async function Aaa() {
       categoriesMap.get(row.category_id).links.push({
         name: row.bookmark_name,
         icon: row.bookmark_icon,
-        site: row.bookmark_site
+        site: row.bookmark_site,
+        id:   row.bookmark_id
       });
     }
   });
 
+  console.log(JSON.stringify(Array.from(categoriesMap.values())))
+
   return Array.from(categoriesMap.values())
 }
+
+Aaa()
+
