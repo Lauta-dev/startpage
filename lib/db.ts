cf0b17f8b1f28e1a8877fa518aea3c92c52e0f5f
lauta-dev <altairdiaz245@gmail.com>
2026-02-17 22:27:54 -0300

    fix: usa las variables de entorno

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,11 @@
 // Prueba este import específico si el general falla en Bun
 import { createClient } from "@libsql/client"; 
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const client = createClient({
-  url: "file:mydatabase.db",
+  url: isDev ? "file:mydatabase.db" : process.env.TURSO_DATABASE_URL,
+  authToken: isDev ? undefined : process.env.TURSO_AUTH_TOKEN,
 });
 
 export default client
