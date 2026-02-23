6b2de52c111d89a2cb3c39731fd086b0be5b3e54
lauta-dev <altairdiaz245@gmail.com>
2026-02-23 00:22:00 -0300

    force dynamic in /bookmakrs

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,6 +1,8 @@
 import getBookmarks from '@/actions/getBookmark';
 import { BookmarkLayout } from '@/components/bookmarks/BookmarkLayout';
 
+export const dynamic = 'force-dynamic';
+
 export default async function BookmarksPage() {
   const bookmarks = await getBookmarks();
   return <BookmarkLayout bookmarks={bookmarks} />;
